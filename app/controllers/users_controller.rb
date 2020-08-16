1f12a4893dcadbf2584b9a68e25815cde8431ffe
Luca <lucakanyo@protonmail.com>
2020-08-15 21:22:37 -0400

    created users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,17 @@ class UsersController < ApplicationController
     end 
 
     def create 
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else 
+            render :new 
+        end 
     end 
 
     def show 
+        @user = User.find_by_id(params[:id])
     end 
 
     private 
