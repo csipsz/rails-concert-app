b69ef861be468fd0883dcd600b2985a12ef26ee9
Luca <lucakanyo@protonmail.com>
2020-08-16 12:40:52 -0400

    wrote out ticket and concert controller actions

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,50 @@
 class ConcertsController < ApplicationController
+    before_action :set_concert, only: [:show, :edit, :update, :destroy]
+    def index 
+        @concerts = Concert.all
+    end 
+
+    def new 
+        @concert = Concert.new
+    end 
+
+    def create 
+        @concert = Concert.new(concert_params)
+        if @concert.save 
+            redirect_to concert_path(@concert)
+        else 
+            render :new 
+        end 
+    end 
+
+    def show 
+    end 
+
+    def edit 
+    end 
+
+    def update 
+        @concert.update(concert_params)
+        if @concert.save 
+            redirect_to concert_path(@concert)
+        else 
+            render :edit 
+        end 
+    end 
+
+    def destroy 
+        @concert.destroy 
+        redirect_to concerts_path
+    end 
+
+
+    private 
+
+    def set_concert 
+        @concert = Concert.find_by_id(params[:id])
+    end 
+
+    def concert_params
+        params.require(:concert).permit(:location, :artist_id)
+    end 
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,58 @@
 class TicketsController < ApplicationController
-end
+        before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+        def index 
+            if params[:user_id]
+                @tickets = User.find(params[:user_id]).tickets 
+            else 
+                @tickets = Ticket.all
+            end 
+        end 
+    
+        def new 
+            @ticket = Ticket.new()
+        end 
+    
+        def create 
+            @ticket = Ticket.new(ticket_params)
+            @ticket.user = current_user
+            @ticket.set_price
+            if @ticket.save 
+                redirect_to ticket_path(@ticket)
+            else 
+                render :new 
+            end 
+        end 
+    
+        def show 
+        end 
+    
+        def edit 
+        end 
+    
+        def update 
+            @ticket.update(ticket_params)
+            @ticket.user_id = current_user
+            if @ticket.save 
+                redirect_to ticket_path(@ticket)
+            else 
+                render :edit 
+            end 
+        end 
+    
+        def destroy 
+            @ticket.destroy 
+            redirect_to  tickets_path
+        end 
+    
+    
+        private 
+    
+        def set_ticket 
+            @ticket = Ticket.find_by_id(params[:id])
+        end 
+    
+        def ticket_params
+            params.require(:ticket).permit(:category, :quantity, :user_id, :concert_id)
+        end 
+    
+    end
