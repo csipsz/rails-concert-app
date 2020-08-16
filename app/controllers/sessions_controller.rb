39bc8c5a0c4068d3c06580538a43dcae263721c3
Luca <lucakanyo@protonmail.com>
2020-08-15 21:40:07 -0400

    added login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,21 @@ class SessionsController < ApplicationController
     end 
 
     def new 
+        @user = User.new 
     end 
 
     def create 
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else 
+            redirect_to root_path 
+        end 
     end 
 
     def destroy 
+        session.clear 
+        redirect_to root_path
     end 
 end
