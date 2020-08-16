beb4c49ca6cad827a4b118233070a92f5b0e8050
Luca <lucakanyo@protonmail.com>
2020-08-16 12:43:09 -0400

    added basic validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    
     has_many :tickets
     has_many :comments 
     has_many :concerts, through: :tickets
+
+
 end
