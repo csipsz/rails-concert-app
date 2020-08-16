a0a080df250712f85c2546406732bc4dfc5f8b1c
Luca <lucakanyo@protonmail.com>
2020-08-15 21:52:11 -0400

    added omniauth callback route to routes and githubcreate action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,9 @@ class SessionsController < ApplicationController
         end 
     end 
 
+    def githubcreate 
+    end 
+
     def destroy 
         session.clear 
         redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :users
-  
+
   root 'sessions#home'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  match '/auth/github/callback', to: 'sessions#githubcreate', via: [:get, :post]
 end
