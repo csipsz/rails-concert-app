71f19d96d5b10c85a73393044cbe320aea441477
Luca <lucakanyo@protonmail.com>
2020-08-15 21:09:17 -0400

    creating users resource and sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+    def home 
+    end 
+
+    def new 
+    end 
+
+    def create 
+    end 
+
+    def destroy 
+    end 
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+    def new 
+        @user = User.new 
+    end 
+
+    def create 
+    end 
+
+    def show 
+    end 
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :email, :password)
+    end 
+end
