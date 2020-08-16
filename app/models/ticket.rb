160ab47107db4c28f457921bcd0b48cda64bdd9d
Luca <lucakanyo@protonmail.com>
2020-08-16 12:41:54 -0400

    added business logic methods to the ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,16 @@
 class Ticket < ApplicationRecord
     belongs_to :user
     belongs_to :concert
+
+    def set_price
+        self.price = self.category * 7
+    end 
+
+    def total_cost 
+        self.price * self.quantity
+    end 
+
+    def total_with_tax 
+        (self.total_cost * 0.06) + self.total_cost
+    end 
 end
