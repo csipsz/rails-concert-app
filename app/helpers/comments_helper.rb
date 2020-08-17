95df1124177c85f5eed201d123eada2a00b8d3d8
Luca <lucakanyo@protonmail.com>
2020-08-17 12:56:14 -0400

    added a comment helper to handle the unnested new comment path with a collection dropdown

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,9 @@
 module CommentsHelper
+    def concert_id_field(comment)
+        if comment.concert.nil?
+          select_tag "comment[concert_id]", options_from_collection_for_select(Concert.all, :id, :performer)
+        else
+          hidden_field_tag "comment[concert_id]", comment.concert_id
+        end
+      end
 end
