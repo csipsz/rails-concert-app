db2c3bc261d123784fb167cd5ad8459854dd256d
Luca <lucakanyo@protonmail.com>
2020-08-17 16:01:40 -0400

    moved logic from view template to comments_helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,4 +1,5 @@
 module CommentsHelper
+
     def concert_id_field(comment)
         if comment.concert.nil?
           select_tag "comment[concert_id]", options_from_collection_for_select(Concert.all, :id, :performer)
@@ -6,4 +7,11 @@ module CommentsHelper
           hidden_field_tag "comment[concert_id]", comment.concert_id
         end
       end
+
+    def show_delete(comment) 
+        if comment.user == current_user
+            button_to "Delete Comment", comment_path(comment), method: :delete
+        end
+    end 
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
     belongs_to :concert
     belongs_to :user, optional: true
     validates :content, length: {minimum: 3}
+
+    def commenter 
+        self.user.username 
+    end 
 end
